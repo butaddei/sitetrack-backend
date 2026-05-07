ef3075561c1fe6624e4ab8ef070bf84489a91e9c
butaddei <butaddei@gmail.com>
2026-05-07 23:02:40 +1000

    fix: add config plugin to resolve SubscriptionPeriod ambiguity in Xcode build

---
+++ artifacts/mobile/plugins/withSubscriptionPeriodFix.js
@@ -0,0 +1,38 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const withSubscriptionPeriodFix = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = fs.readFileSync(podfilePath, 'utf8');
+
+      const fix = `
+  # Fix: 'SubscriptionPeriod' is ambiguous between StoreKit and react-native-purchases
+  installer.pods_project.targets.each do |target|
+    if target.name == 'react-native-purchases'
+      target.build_configurations.each do |config|
+        config.build_settings['SWIFT_INSTALL_OBJC_HEADER'] = 'NO'
+      end
+    end
+  end
+`;
+
+      if (contents.includes('post_install do |installer|')) {
+        contents = contents.replace(
+          'post_install do |installer|',
+          `post_install do |installer|\n${fix}`
+        );
+      } else {
+        contents += `\npost_install do |installer|\n${fix}\nend\n`;
+      }
+
+      fs.writeFileSync(podfilePath, contents);
+      return config;
+    },
+  ]);
+};
+
+module.exports = withSubscriptionPeriodFix;
